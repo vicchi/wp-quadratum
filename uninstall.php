7f1ffdd4038751d4887e5d1e9326ceb53fe2d0ec
vicchi <gary@vicchi.org>
2013-07-23 14:57:32 +0100

    Clean up widget settings on uninstall.

--- uninstall.php
+++ uninstall.php
@@ -1,11 +1,12 @@
 <?php
 
-if (defined ('WP_UNINSTALL_PLUGIN')) {
-	delete_option ('wp_quadratum_settings');
+if (defined('WP_UNINSTALL_PLUGIN')) {
+	delete_option('wp_quadratum_settings');
+	delete_option('widget_wp-quadratum_widget');
 }
 
 else {
-	exit ();
+	exit();
 }
 
 ?>
